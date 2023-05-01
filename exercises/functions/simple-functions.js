91952fd7a8e320e8c752ae71ede1052c77bb6234
Jonathan Tran <trandingtopics@gmail.com>
2023-05-01 11:35:52 -0400

    initial commit

---
+++ exercises/functions/simple-functions.js
@@ -0,0 +1,15 @@
+function favoriteThings(){
+    console.log("Jon's favorite things are: " );
+    console.log("Jon's favorite movie is Cat in the Hat.");
+    console.log("Jon's favorite band is Mili.");
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log(`
+    I really like to do stuff
+        and I find it to be less boring to do stuff.`);
+}
+
+whyImHere();
